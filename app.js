a646a0ee3588eb6b6008f92b9ae377b7c061c072
Subhan Huseynov <johndoe@email.com>
2024-09-11 19:25:42 +0400

    v06

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+function display(value) {
+
+    document.getElementById("display").value += value;
 }
-function clearDisplay() {
-    document.getElementById('display').value = '';
+function AC(value) {
+    document.getElementById("display").value = ""
+}
+function del(value) {
+    document.getElementById("display").value += value.slice(0, -1);
+
+}
+function del() {
+    let display = document.getElementById("display");
+    let halhazirkideyer = display.value;
+    let yenideyer = display.value.slice(0, -1);
+    display.value = yenideyer;
+}
+function solve() {
+
+
+    const result = document.getElementById('display').value;
+    display.value += result;
+
+}
+function solve() {
+    let display = document.getElementById("display");
+    try {
+        display.value = eval(display.value) || "Geçersiz işlem";
+    } catch {
+        display.value = "Hata";
+    }
 }
